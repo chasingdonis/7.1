bab862ee3720d05b01a810c64562c332bc87872e
chasingdonis <cuixianning@hotmail.com>
2015-10-06 13:38:44 -0500

    done editing

---
+++ script.js
@@ -0,0 +1,14 @@
+var newHead = document.createElement("h2");
+var newPara = document.createElement("p");
+
+document.getElementById("exe").appendChild(newHead);
+document.getElementById("exe").appendChild(newPara);
+
+newHead.innerHTML = "Working with Dom";
+newPara.innerHTML = "Getting to know about Dom, and trying to create one! Have fun!";
+
+var changeFormat = document.getElementById("exe");
+changeFormat.setAttribute("align", "center");
+
+var changeTitle = document.getElementsByTagName("HEADER")[0];
+changeTitle  .setAttribute("align", "center");
